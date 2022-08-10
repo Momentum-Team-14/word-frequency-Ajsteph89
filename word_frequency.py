3a65d7633a9ee1b0eb4482d4abec2f937d7b0699
Ajsteph89 <ajsteph89@gmail.com>
2022-08-10 17:19:12 -0400

    Remove punctuation, set to all lowercase, split strings to individual words, count them

--- word_frequency.py
+++ word_frequency.py
@@ -1,15 +1,37 @@
+from subprocess import list2cmdline
+
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
 
+import string
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    with open(file, 'r') as input_file:
+        #reads file set to variable contents
+        contents = input_file.read()
+        #changes contents to all lower case
+        lower_case = contents.lower()
+        #removes punctuation from lower case contents
+        no_punct = lower_case.translate(str.maketrans('', '', string.punctuation))
+        #split string into separate items in a list
+        list_content = no_punct.split(" ")
+        #establish a dictionary
+        totals = dict()
+        #loop through items in our list "list_contnet"
+        for word in list_content:
+            if word not in totals:
+                totals[word] = 1 #put word in dict w/ count 
+            else:
+                totals[word] += 1 #increase count if word already present
+        print(totals)
 
 
+#don't touch
 if __name__ == "__main__":
     import argparse
     from pathlib import Path
@@ -25,3 +47,4 @@ if __name__ == "__main__":
     else:
         print(f"{file} does not exist!")
         exit(1)
+
