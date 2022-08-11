1afb5f2571907a4ad89092f03321ebdacb28ee87
Ajsteph89 <ajsteph89@gmail.com>
2022-08-11 11:49:33 -0400

    STOP WORDS removed

--- word_frequency.py
+++ word_frequency.py
@@ -14,20 +14,29 @@ def print_word_freq(file):
     with open(file, 'r') as input_file:
         #reads file set to variable contents
         contents = input_file.read()
+        
         #changes contents to all lower case
         lower_case = contents.lower()
+        
         #removes punctuation from lower case contents
-        no_punct = lower_case.translate(str.maketrans('', '', string.punctuation))
+        no_punct = lower_case.translate(str.maketrans("","", string.punctuation))
+        
         #split string into separate items in a list
-        list_content = no_punct.split(" ")
+        list_content = no_punct.split()
+
+        #filter out STOP_WORDS form our list
+        less_words = [item for item in list_content if item not in STOP_WORDS]
+        
         #establish a dictionary
         totals = dict()
+        
         #loop through items in our list "list_contnet"
-        for word in list_content:
+        for word in less_words:
             if word not in totals:
                 totals[word] = 1 #put word in dict w/ count 
             else:
                 totals[word] += 1 #increase count if word already present
+        
         print(totals)
 
 
